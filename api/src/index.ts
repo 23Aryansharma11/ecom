b94aa9d86f7e419bdba784cf59e22c039360acfd
Aryan Sharma <23aryansharma11@gmail.com>
2025-02-10 13:19:59 +0530

    add drizzle

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productRouter from "./routes/products";
 
 const port = process.env.PORT;
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use("/product", productRouter);
 
