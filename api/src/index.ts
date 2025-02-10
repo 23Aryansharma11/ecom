83d63015b6ca6476cf2a6adbcbced504ca21ea1b
Aryan Sharma <23aryansharma11@gmail.com>
2025-02-10 17:58:00 +0530

    add configs

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json, urlencoded } from "express";
 import productRouter from "./routes/products/index.js";
 import authRoutes from "./routes/auth/index.js";
+import serverless from "serverless-http";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,9 @@ app.use(urlencoded({ extended: true }));
 
 app.use("/product", productRouter);
 app.use("/auth", authRoutes);
-app.listen(port, () => {
-  console.log(`Server running on Port: ${port}`);
-});
+if(process.env.NODE_ENV === "dev"){ 
+  app.listen(port, () => {
+    console.log(`Server running on Port: ${port}`);
+  }); 
+}
+export const handler = serverless(app);
